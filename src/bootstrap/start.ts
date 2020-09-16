8dbc63c302dc34b26399490f6e03f48071bef275
Pradnyana28 <kadek.pradnyana@gmail.com>
2020-09-16 11:03:19 +0800

    use testing instead of test

--- src/bootstrap/start.ts
+++ src/bootstrap/start.ts
@@ -8,12 +8,18 @@ import chalk from 'chalk';
  */
 const start = (app: Application) => {
     app.listen(app.get('port'), () => {
-        if (app.get('env') != 'test') {
-            console.log('%s %s Serve at http://localhost:%d in %s mode', chalk.green('✓'), chalk.blueBright('INIT'), app.get('port'), chalk.greenBright(app.get('env')));
+        if (app.get('env') != 'testing') {
+            console.log(
+                '%s %s Serve at http://localhost:%d in %s mode',
+                chalk.green('✓'),
+                chalk.blueBright('INIT'),
+                app.get('port'),
+                chalk.greenBright(app.get('env'))
+            );
         }
     });
 
     return app;
-}
+};
 
 export default start;
